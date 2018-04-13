9e6bad505130bf8e0e489c352d6336e48b58f707
zlikun <zlikun-dev@hotmail.com>
2018-04-13 16:09:22 +0800

    初始化工程

---
+++ src/main/java/com/zlikun/nginx/NginxWebApplication.java
@@ -0,0 +1,13 @@
+package com.zlikun.nginx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NginxWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NginxWebApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zlikun/nginx/controller/NginxController.java
@@ -0,0 +1,33 @@
+package com.zlikun.nginx.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+/**
+ * @author zlikun <zlikun-dev@hotmail.com>
+ * @date 2018/4/13 15:24
+ */
+@Slf4j
+@RestController
+public class NginxController {
+
+    @GetMapping("/")
+    public ResponseEntity<String> index(HttpServletRequest request) {
+        log.info("Hello, Nginx!");
+
+        // 打印请求消息头
+        Enumeration<String> headers = request.getHeaderNames();
+        while (headers.hasMoreElements()) {
+            String name = headers.nextElement();
+            log.info("header - name = {}, value = {}", name, request.getHeader(name));
+        }
+
+        return ResponseEntity.ok("Hello, Nginx !");
+    }
+
+}
